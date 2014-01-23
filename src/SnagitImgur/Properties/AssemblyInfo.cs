d679023522bd4d858b0e29332b3ddb0439ec143b
Igal Tabachnik <hmemcpy@gmail.com>
2014-01-23 20:53:42 +0200

    Adding project skeleton

---
+++ src/SnagitImgur/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SnagitImgur")]
+[assembly: AssemblyDescription("An unofficial plugin (\"accessory\") for Techsmith® Snagit® that allows sending captured images to imgur.com")]
+[assembly: AssemblyCompany("Igal Tabachnik")]
+[assembly: AssemblyProduct("Send to imgur.com")]
+[assembly: AssemblyCopyright("Copyright © Igal Tabachnik 2011-2014")]
+
+[assembly: ComVisible(true)]
+
+[assembly: AssemblyVersion("0.5.0")]
+[assembly: AssemblyFileVersion("0.5.0")]

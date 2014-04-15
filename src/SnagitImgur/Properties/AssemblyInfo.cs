a96b64fe9f8abe3e456badbcb587a1c91edf99ee
Igal Tabachnik <hmemcpy@gmail.com>
2014-04-15 19:27:38 +0300

    Modifying version number

--- src/SnagitImgur/Properties/AssemblyInfo.cs
+++ src/SnagitImgur/Properties/AssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.5.0")]
-[assembly: AssemblyFileVersion("0.5.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]

3698d336691466e564c61487d4cb5d4ffa90f0ec
Igal Tabachnik <hmemcpy@gmail.com>
2015-05-22 23:38:13 +0300

    Bumping version

--- src/SnagitImgur/Properties/AssemblyInfo.cs
+++ src/SnagitImgur/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("An unofficial plugin (\"accessory\") for Techsmith® Snagit® that allows sending captured images to imgur.com")]
 [assembly: AssemblyCompany("Igal Tabachnik")]
 [assembly: AssemblyProduct("Send to imgur.com")]
-[assembly: AssemblyCopyright("Copyright © Igal Tabachnik 2011-2014")]
+[assembly: AssemblyCopyright("Copyright © Igal Tabachnik 2011-2015")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.0")]
 
 [assembly: InternalsVisibleTo("SnagitImgur.Tests")]

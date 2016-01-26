2e78673e2560044b8bcc2cfb812a1fbb3fdc8938
ebmp19 <notmyemail@hotmail.com>
2016-01-27 06:39:42 +1100

    Fixed crash and UI for HDPI Displays
    I removed the toast notification as this doesn't work at all and causes
    the application to crash the second time the button is pressed on
    windows 10.  Updated the UI to be usable on a HDPI display and removed
    pointless close buttons.

--- src/SnagitImgur/Dialogs/AccountForm.Designer.cs
+++ src/SnagitImgur/Dialogs/AccountForm.Designer.cs
@@ -28,85 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.PictureBox pictureBox1;
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AccountForm));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.brnClose = new System.Windows.Forms.Button();
-            this.btnAuthenticate = new System.Windows.Forms.Button();
             this.lblAccountName = new System.Windows.Forms.Label();
-            new System.Windows.Forms.LinkLabel();
-            pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(pictureBox1)).BeginInit();
+            this.btnAuthenticate = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            pictureBox1.Location = new System.Drawing.Point(6, 19);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new System.Drawing.Size(59, 61);
-            pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
             // 
             // groupBox1
             // 
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnAuthenticate);
             this.groupBox1.Controls.Add(this.lblAccountName);
-            this.groupBox1.Controls.Add(pictureBox1);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(336, 86);
+            this.groupBox1.Size = new System.Drawing.Size(385, 151);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Account";
             // 
-            // brnClose
+            // lblAccountName
             // 
-            this.brnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.brnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.brnClose.Location = new System.Drawing.Point(273, 104);
-            this.brnClose.Name = "brnClose";
-            this.brnClose.Size = new System.Drawing.Size(75, 23);
-            this.brnClose.TabIndex = 1;
-            this.brnClose.Text = "&Close";
-            this.brnClose.UseVisualStyleBackColor = true;
+            this.lblAccountName.AutoSize = true;
+            this.lblAccountName.Location = new System.Drawing.Point(185, 0);
+            this.lblAccountName.Name = "lblAccountName";
+            this.lblAccountName.Size = new System.Drawing.Size(128, 30);
+            this.lblAccountName.TabIndex = 1;
+            this.lblAccountName.Text = "Anonymous";
             // 
             // btnAuthenticate
             // 
             this.btnAuthenticate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnAuthenticate.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAuthenticate.Location = new System.Drawing.Point(176, 104);
+            this.btnAuthenticate.Location = new System.Drawing.Point(57, 59);
             this.btnAuthenticate.Name = "btnAuthenticate";
-            this.btnAuthenticate.Size = new System.Drawing.Size(91, 23);
+            this.btnAuthenticate.Size = new System.Drawing.Size(256, 92);
             this.btnAuthenticate.TabIndex = 2;
             this.btnAuthenticate.Text = "Authenticate";
             this.btnAuthenticate.UseVisualStyleBackColor = true;
             this.btnAuthenticate.Click += new System.EventHandler(this.btnAuthenticate_Click);
             // 
-            // lblAccountName
-            // 
-            this.lblAccountName.AutoSize = true;
-            this.lblAccountName.Location = new System.Drawing.Point(71, 30);
-            this.lblAccountName.Name = "lblAccountName";
-            this.lblAccountName.Size = new System.Drawing.Size(68, 13);
-            this.lblAccountName.TabIndex = 1;
-            this.lblAccountName.Text = "Anonymous";
-            // 
             // AccountForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.brnClose;
-            this.ClientSize = new System.Drawing.Size(360, 139);
-            this.Controls.Add(this.btnAuthenticate);
-            this.Controls.Add(this.brnClose);
+            this.ClientSize = new System.Drawing.Size(409, 204);
             this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -116,7 +83,6 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "imgur.com";
             this.Load += new System.EventHandler(this.AccountFormLoad);
-            ((System.ComponentModel.ISupportInitialize)(pictureBox1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -126,7 +92,6 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button brnClose;
         private System.Windows.Forms.Button btnAuthenticate;
         private System.Windows.Forms.Label lblAccountName;
     }
--- src/SnagitImgur/Dialogs/PinCodeForm.Designer.cs
+++ src/SnagitImgur/Dialogs/PinCodeForm.Designer.cs
@@ -31,7 +31,6 @@
             System.Windows.Forms.GroupBox groupBox1;
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.txtPinCode = new System.Windows.Forms.TextBox();
-            this.btnCancel = new System.Windows.Forms.Button();
             this.btnAccept = new System.Windows.Forms.Button();
             groupBox1 = new System.Windows.Forms.GroupBox();
             groupBox1.SuspendLayout();
@@ -47,7 +46,7 @@
             groupBox1.Controls.Add(this.txtPinCode);
             groupBox1.Location = new System.Drawing.Point(12, 12);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new System.Drawing.Size(260, 53);
+            groupBox1.Size = new System.Drawing.Size(497, 109);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "Enter pin code";
@@ -55,7 +54,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::SnagitImgur.Properties.Resources.spinning;
-            this.pictureBox1.Location = new System.Drawing.Point(236, 24);
+            this.pictureBox1.Location = new System.Drawing.Point(166, 9);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(16, 16);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -67,30 +66,19 @@
             // 
             this.txtPinCode.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtPinCode.Location = new System.Drawing.Point(6, 21);
+            this.txtPinCode.Location = new System.Drawing.Point(0, 36);
             this.txtPinCode.MaxLength = 30;
             this.txtPinCode.Name = "txtPinCode";
-            this.txtPinCode.Size = new System.Drawing.Size(248, 22);
+            this.txtPinCode.Size = new System.Drawing.Size(485, 37);
             this.txtPinCode.TabIndex = 0;
             this.txtPinCode.TextChanged += new System.EventHandler(this.txtPinCode_TextChanged);
-            // 
-            // btnCancel
-            // 
-            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(197, 71);
-            this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
-            this.btnCancel.TabIndex = 2;
-            this.btnCancel.Text = "&Cancel";
-            this.btnCancel.UseVisualStyleBackColor = true;
             // 
             // btnAccept
             // 
             this.btnAccept.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnAccept.Location = new System.Drawing.Point(116, 71);
+            this.btnAccept.Location = new System.Drawing.Point(168, 110);
             this.btnAccept.Name = "btnAccept";
-            this.btnAccept.Size = new System.Drawing.Size(75, 23);
+            this.btnAccept.Size = new System.Drawing.Size(178, 45);
             this.btnAccept.TabIndex = 1;
             this.btnAccept.Text = "&OK";
             this.btnAccept.UseVisualStyleBackColor = true;
@@ -99,12 +87,10 @@
             // PinCodeForm
             // 
             this.AcceptButton = this.btnAccept;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(284, 106);
+            this.ClientSize = new System.Drawing.Size(521, 162);
             this.Controls.Add(this.btnAccept);
-            this.Controls.Add(this.btnCancel);
             this.Controls.Add(groupBox1);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
@@ -121,7 +107,6 @@
         #endregion
 
         private System.Windows.Forms.TextBox txtPinCode;
-        private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.Button btnAccept;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
